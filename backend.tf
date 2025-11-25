b88d999d54ce67812f7f52c6aa060177b547d7a1
EC2 Default User <ec2-user@ip-172-31-11-229.ec2.internal>
2025-11-25 22:59:06 +0000

    Ajustando configuracoes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-vini-clc14-remote"
+    bucket       = "vini-terraform-clc14"
     key          = "network/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
